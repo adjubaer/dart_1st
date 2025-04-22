6959f35eec4c1572c823b94d7ea5953836c5036e
Md Jubaer Islam <mdjubaerislam2020@gmail.com>
2025-04-22 23:06:28 +0600

    git update

--- bin/module 6/git commsnd.dart
+++ bin/module 6/git commsnd.dart
@@ -1,5 +1,11 @@
+
+//git inti
 //git status : kon kon file update korechi seta show kore
 //git branch : kon kon branch ase seegula show korbe and je branch a active asi shay branch star  diye mark kore dekhabe
 //git checkout -b module_1 :  create a new branch and  switch to new branch module_1
 //git add "file name" : specific kno file add hobe
 //git add . : jotogula file update hoise sobgula module add hobe
+//git commit -m "any comment" : projrect  er moddhe ki ki change korechi seta dekhabe
+//git push origin branch name :
+//git remote add origin "link" : link er oikhane github er link ta dite hobe ja project er sathe connect hoy
+//git push u origin main :
